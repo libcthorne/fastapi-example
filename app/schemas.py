2a87bf0c0a4cc464a8be144f8b475a15227e491b
Christopher Thorne <c.thorne@reckondigital.com>
2022-10-31 13:07:53 +0000

    Move to using countries model

--- app/schemas.py
+++ app/schemas.py
@@ -1,35 +1,17 @@
 from pydantic import BaseModel
 
 
-class ItemBase(BaseModel):
+class CountryBase(BaseModel):
     title: str
     description: str | None = None
 
 
-class ItemCreate(ItemBase):
+class CountryCreate(CountryBase):
     pass
 
 
-class Item(ItemBase):
+class Country(CountryBase):
     id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
 
     class Config:
         orm_mode = True
